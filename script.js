1cfeb5fee17f5289aa6b087ea6f87f1f5c8af72d
Thanos Ioannidis <thanos.p.ioannidis@gmail.com>
2026-01-20 04:34:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const header = document.getElementById("header");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 40) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
+  header.classList.toggle("scrolled", window.scrollY > 20);
 });
