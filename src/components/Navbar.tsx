7deb85a26a64a60ddb52a5aa55a4ef8d638d42d8
Thanos Ioannidis <thanos.p.ioannidis@gmail.com>
2026-01-21 13:08:04 +0200

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <span>YourName</span>
+      <div>
+        <a href="#about">About</a>
+        <a href="#experience">Experience</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
