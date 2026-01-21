68f1093bbd2783a65dc865a6f0bc4aa4f7bb5be1
Thanos Ioannidis <thanos.p.ioannidis@gmail.com>
2026-01-21 13:20:48 +0200

    Create Skills.tsx

---
+++ src/sections/Skills.tsx
@@ -0,0 +1,12 @@
+export default function Skills() {
+  return (
+    <section id="skills">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>TypeScript</li>
+      </ul>
+    </section>
+  );
+}
